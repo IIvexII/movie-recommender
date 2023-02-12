cab8cc383570389d143f0bb9f0bbc87d4c7204b3
IIvexII <mrh4ck3r1738@gmail.com>
2023-02-12 08:18:14 +0500

    feat: add apis to get those recommended movies in json format

--- apis/main.py
+++ apis/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
-from deta import Deta
+import pickle
 
 app = FastAPI()
 
+# loading the data
+movies = pickle.load(open("data/movies.pkl", "rb"))
+similarity = pickle.load(open("data/similarities.pkl", "rb"))
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/{movie_name}")
+async def recommend_movies(movie_name):
+    movie = movies[movies["title"] == movie_name]
+    if (len(movie) > 0):
+        movie_index = movie.index[0]
+        distance = similarity[movie_index]
+
+        movie_list = sorted(list(enumerate(distance)),
+                            reverse=True, key=lambda tupple: tupple[1])[1:6]
+
+        return {"movies": [movies.iloc[movie[0]].title for movie in movie_list]}
+    else:
+        return {"err_code": 404, "movies": []}
